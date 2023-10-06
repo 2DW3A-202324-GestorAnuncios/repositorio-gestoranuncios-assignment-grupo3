b932bd125b2e24e4f691eec503f53c3ab670240f
DwayneLucas <dwaynelucas1119@gmail.com>
2023-10-06 12:49:41 +0200

    Comprobacion de creacion de cuenta

--- Cuentas/crear_cuenta.php
+++ Cuentas/crear_cuenta.php
@@ -19,7 +19,7 @@
     } else {
         $usu = test_input($_POST["usuario"]);
         // check if name only contains letters and whitespace
-        if (!preg_match("/^[a-zA-Z][0-9]*$/",$usu)) {
+        if (!preg_match("/^[a-zA-Z0-9]*$/",$usu)) {
         $usuErr = "Introduce solo letras y numeros";
         }
     }
@@ -37,12 +37,11 @@
     if (empty($_POST["contraseña1"])) {
         $contraErr = "Introduce una contraseña";
     } else {
-        $contra = test_input($_POST["website"]);
         
     }
 
     if (($_POST["contraseña1"])!=($_POST["contraseña2"])) {
-        $contra2 = "Las contraseñas tienen que ser identicas";
+        $contra2Err = "Las contraseñas no coinciden";
     }
 
     if (empty($_POST["gender"])) {
@@ -61,29 +60,33 @@
     ?>
     <div class="form_crear_cuenta">
         <img src="../imagenes/logo.png" alt="logo" class="logo_inicio_sesion">
-        <form action="../index.php" class="crear_cuenta" method="post" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>>
-            <div>
-                <p>Usuario:</p>
-                <input type="text" class="input_text" maxlength="15" value="<?php echo $usu;?>" name="usuario"><br><br>
-                <span class="error"> <?php echo $usuErr;?></span>
-            </div>
-            <div>
-                <p>Correo:</p>
-                <input type="mail" class="input_text" value="<?php echo $email;?>" name="email"><br><br>
-                <span class="error"> <?php echo $emailErr;?></span>
-            </div>
-            <div>
-                <p>Contraseña:</p>
-                <input type="text" class="input_text" name="contra1" value="<?php echo $contra1;?>" name="contraseña1"><br><br>
-                <span class="error"> <?php echo $contra1Err;?></span>
+        <form action=""  method="post" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>>
+            <div class="crear_cuenta">
+                <div>
+                    <p>Usuario:</p>
+                    <input type="text" class="input_text" maxlength="15" value="<?php echo $usu;?>" name="usuario"><br>
 
-            </div>
-            <div>
-                <p>Confirmar contraseña:</p>
-                <input type="text" class="input_text" name="contra2" value="<?php echo $contra2;?>" name="cotnraseña2"><br><br>
-                <span class="error"> <?php echo $contra2Err;?></span>
+                    <span class="error"> <?php echo $usuErr;?></span>
+                </div>
+                <div>
+                    <p>Correo:</p>
+                    <input type="mail" class="input_text" value="<?php echo $email;?>" name="email"><br>
+                    <span class="error"> <?php echo $emailErr;?></span>
+                </div>
+                <div>
+                    <p>Contraseña:</p>
+                    <input type="text" class="input_text" name="contraseña1" value="<?php echo $contra1;?>" name="contraseña1"><br>
+                    <span class="error"> <?php echo $contra1Err;?></span>
 
+                </div>
+                <div>
+                    <p>Confirmar contraseña:</p>
+                    <input type="text" class="input_text" name="contraseña2" value="<?php echo $contra2;?>" name="cotnraseña2"><br>
+                    <span class="error"> <?php echo $contra2Err;?></span>
+
+                </div>
             </div>
+            <br>
             <div class="terminos_crear_cuenta">
                 <input type="checkbox" name="terminos" class="terminos_checkbox" id="terminos_crear_cuenta" value="<?php echo $terms;?>" name="terminos">
                 <label class="terminos_checkbox" for="terminos_crear_cuenta">Acepto los terminos y condiciones</label><br><br>
@@ -93,6 +96,16 @@
             <input type="submit" value="Crear Cuenta" name="submit" class="boton">
 
             </form>
+
+            <?php
+            echo "<h2>Your Input:</h2>";
+            echo $usu;
+            echo "<br>";
+            echo $email;
+            echo "<br>";
+            echo $contra2;
+            echo "<br>";
+            ?>
             
 
     </div>
