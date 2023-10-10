75f12f505c89b7e10449201ea1f19f00bbd40b20
aaketza <aketzacb@gmail.com>
2023-10-10 10:32:21 +0200

    cambios en index y noticias

--- index.php
+++ index.php
@@ -17,25 +17,123 @@
       include("header.php");
     ?>
 
-    <section><br>
-        <div class="noticias2">
-            <button id="anterior" class="carousel-button">&#10094;</button>
-            <div class="noticia">
-                <img src="img/noticia_ansu.webp" alt="Noticia Ansu" class="imagen-noticia">
-                <h2 class="titulo-noticia">Noticia Ansu</h2>
-            </div>
-            <div class="noticia">
-                <img src="img/noticia_barca.webp" alt="Noticia Barca" class="imagen-noticia">
-                <h2 class="titulo-noticia">Noticia Deudas</h2>
-            </div>
-            <div class="noticia">
-                <img src="img/noticia_mvps_euroliga.webp" alt="MVPs EuroLiga" class="imagen-noticia">
-                <h2 class="titulo-noticia">MVPs EuroLiga</h2>
-            </div>
-            <button id="siguiente" class="carousel-button">&#10095;</button>
+    <section id="ultimas-noticias" class="seccion-destacada"><br>
+        <div class="seccion-titulo">
+            <h2 class="titulo-llamativo">¡Mantente al Día!</h2><br>
         </div>
+        <!-- Slideshow container -->
+        <div class="slideshow-container">
+            <!-- Full-width images with number and caption text -->
+            <div class="mySlides fade">
+                <div class="numbertext">1 / 3</div>
+                <img src="img/noticia_barca.webp" style="width:100%">
+                <div class="text">El Barça logró imponerse a todos sus fantasmas en Oporto</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">2 / 3</div>
+                <img src="img/noticia_ansu.webp" style="width:100%">
+                <div class="text">El plan del Brighton para poner a tope a Ansu Fati: "No queremos presionarle"</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">3 / 3</div>
+                <img src="img/noticia_mvps_euroliga.webp" style="width:100%">
+                <div class="text">Las estrellas que aspiran a coronarse MVP de la Euroliga</div>
+            </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <br>
+
+        <!-- The dots/circles -->
+        <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+
+        <script>
+        let slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {
+                slideIndex = 1;
+            }
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+
+        // Añade esta función para cambiar de diapositiva cada 10 segundos
+        function autoSlide() {
+            plusSlides(1);
+        }
+
+        // Llama a autoSlide cada 10 segundos (10000 milisegundos)
+        setInterval(autoSlide, 10000);
+        </script>
+
+        <a href="anuncio.php"> <button id="ver-mas-anuncios" class="ver-mas-button">Ver Más Anuncios</button>
     </section><br>
 
+    <section id="anuncios-mas-visitados" class="seccion-destacada">
+        <div class="seccion-contenido">
+            <h2 class="titulo-llamativo">Descubre lo Más Popular</h2>
+            <!-- Aquí puedes mostrar tus tres anuncios más visitados -->
+            <div class="productos2">
+                <div class="producto2">
+                    <img src="img/primeraEquip.webp" alt="Producto 1" />
+                    <h2>Camiseta Hombre Primera Equipacion</h2>
+                    <p>Descripción del Producto 1.</p>
+                    <p>85.00€</p>
+                    <button>Comprar</button>
+                </div>
+                <div class="producto2">
+                    <img src="img/segundaEquip.webp" alt="Producto 2" />
+                    <h2>Camiseta Hombre Primera Equipacion</h2>
+                    <p>Descripción del Producto 1.</p>
+                    <p>85.00€</p>
+                    <button>Comprar</button>
+                </div>
+                <!-- Agrega más productos aquí -->
+                <div class="producto2">
+                    <img src="img/segundaEquip.webp" alt="Producto 2" />
+                    <h2>Camiseta Hombre Primera Equipacion</h2>
+                    <p>Descripción del Producto 1.</p>
+                    <p>85.00€</p>
+                    <button>Comprar</button>
+                </div>
+                <!-- Agrega más productos aquí -->
+            </div>
+        </div>
+
+        <a href="anuncio.php"> <button id="ver-mas-anuncios" class="ver-mas-button">Ver Más Anuncios</button>
+        </a>
+        </div>
+    </section>
     <?php
       include('footer.php');
     ?>
--- noticia.php
+++ noticia.php
@@ -17,10 +17,58 @@
       include("header.php");
     ?>
 
+    <div class="container">
+        <div class="filtro">
+            <h3>Filtrar por Categoría</h3>
+            <form id="filtroForm">
+                <label class="filtro-label">
+                    <input type="radio" name="categoria" value="deportes">
+                    <img src="img/categorias_noticias/deporte.png" alt="Deportes">
+                    <h5>Deportes</h5>
+                </label><br>
+                <label class="filtro-label">
+                    <input type="radio" name="categoria" value="economia">
+                    <img src="img/categorias_noticias/economia.png" alt="Economía">
+                    <h5>Economia</h5>
+                </label><br>
+                <label class="filtro-label">
+                    <input type="radio" name="categoria" value="arte">
+                    <img src="img/categorias_noticias/arte.png" alt="Arte">
+                    <h5>Arte</h5>
+                </label><br>
+                <label class="filtro-label">
+                    <input type="radio" name="categoria" value="tiempo">
+                    <img src="img/categorias_noticias/tiempo.png" alt="Tiempo">
+                    <h5>Tiempo</h5>
+                </label><br>
+            </form>
+        </div>
+        <div class="noticias3">
+            <h2>Noticias</h2>
+            <div id="noticiasContainer" class="noticias-container">
+                <!-- Aquí se cargarán las noticias dinámicamente -->
+                <div class="noticia3 categoria-deportes">
+                    <img src="img/noticia_ansu.webp" alt="Noticia Ansu" class="imagen-noticia3">
+                    <h2 class="titulo-noticia3">Noticia Ansu</h2>
+                </div>
+                <div class="noticia3 categoria-arte">
+                    <img src="img/noticia_barca.webp" alt="Noticia Barca" class="imagen-noticia3">
+                    <h2 class="titulo-noticia3">Noticia Deudas</h2>
+                </div>
+                <div class="noticia3 categoria-economia">
+                    <img src="img/noticia_mvps_euroliga.webp" alt="MVPs EuroLiga" class="imagen-noticia3">
+                    <h2 class="titulo-noticia3">MVPs EuroLiga</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <?php
       include('footer.php');
     ?>
+
+
 </body>
 
 </html>
