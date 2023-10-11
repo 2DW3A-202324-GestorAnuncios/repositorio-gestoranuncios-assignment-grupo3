af6bdf5b1fb20a5b56584f3a443f91a5fb6dd5d7
LukaCarmona <lukacarmona115@gmail.com>
2023-10-11 10:14:49 +0200

    pagina de anuncios con paginate y algo de estilos

--- anuncio.php
+++ anuncio.php
@@ -1,9 +1,27 @@
 <?php
     include("conexion.php");
 
-    $sqlProductos = "SELECT * FROM producto";
+    $elementosPorPagina = 9;
+    $paginaActual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+
+    // Consulta para obtener el número total de productos
+    $sqlTotalProductos = "SELECT COUNT(*) as total FROM producto";
+    $resultTotalProductos = $conn->query($sqlTotalProductos);
+
+    if (!$resultTotalProductos) {
+        die("Error en la consulta: " . $conn->errorInfo()[2]);
+    }
+
+    $totalProductos = $resultTotalProductos->fetch(PDO::FETCH_ASSOC)['total'];
+
+    $paginasTotales = ceil($totalProductos / $elementosPorPagina);
+
+    // Consulta para obtener los productos de la página actual
+    $inicio = ($paginaActual - 1) * $elementosPorPagina;
+    $sqlProductos = "SELECT * FROM producto LIMIT $inicio, $elementosPorPagina";
     $resultProductos = $conn->query($sqlProductos);
 ?>
+
 <!DOCTYPE html>
 <html lang="es-Es">
 <head>
@@ -29,18 +47,29 @@
                     
                     // Verifica si la URL de la imagen es nula o vacía
                     $imagenAlt = empty($row['foto']) ? 'Sin Foto' : ucfirst($row['nombre_pro']);
-                    
-                    echo '<img src="img/' . $row['foto'] . '" alt="' . htmlspecialchars($imagenAlt) . '">';
-                    echo '<h2>' . $row['nombre_pro'] . '</h2>';
-                    echo '<p>' . $row['descripcion'] . '</p>';
-                    echo '<p><b>' . $row['precio'] . '€</b></p>';
-                    echo '<button>Comprar</button>';
+                        echo '<div class = "imagen-producto">';
+                            echo '<img width = "400px" src="img/' . $row['foto'] . '" alt="' . htmlspecialchars($imagenAlt) . '">';
+                        echo '</div>';
+                        echo '<div class = "contenedor-anuncio">';
+                            echo '<h2>' . $row['nombre_pro'] . '</h2>';
+                            echo '<p>' . $row['descripcion'] . '</p>';
+                            echo '<p><b>' . $row['precio'] . '€</b></p>';
+                            echo '<button>Comprar</button>';
+                        echo '</div>';
                     echo '</div>';
                 }
             ?>
         </div>
     </section>
-    
+    <div id="paginacion">
+        <?php if ($paginaActual > 1): ?>
+            <a href="?pagina=<?php echo $paginaActual - 1; ?>">Anterior</a>
+        <?php endif ?>
+        <?php echo $paginaActual . '/' . $paginasTotales; ?>
+        <?php if ($paginaActual < $paginasTotales): ?>
+            <a class="botonesPagina" href="?pagina=<?php echo $paginaActual + 1; ?>">Siguiente</a>
+        <?php endif ?>
+    </div>
     <?php
         include('footer.php');
     ?>
