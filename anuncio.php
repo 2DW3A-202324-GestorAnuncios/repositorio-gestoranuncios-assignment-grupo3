978d2e8597ae3e6a99fbc8c030403e6b3cf92d27
LukaCarmona <lukacarmona115@gmail.com>
2023-10-16 12:20:51 +0200

    creacion de buscador de anuncios

--- anuncio.php
+++ anuncio.php
@@ -4,22 +4,55 @@
     $elementosPorPagina = 9;
     $paginaActual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
 
-    // Consulta para obtener el número total de productos
-    $sqlTotalProductos = "SELECT COUNT(*) as total FROM anuncio WHERE validado = '1'";
-    $resultTotalProductos = $conn->query($sqlTotalProductos);
+    // Inicializa la variable de búsqueda
+    $busqueda = '';
 
-    if (!$resultTotalProductos) {
-        die("Error en la consulta: " . $conn->errorInfo()[2]);
+    // Verifica si se ha enviado una búsqueda
+    if (isset($_GET['busqueda'])) {
+        $busqueda = $_GET['busqueda'];
     }
 
-    $totalProductos = $resultTotalProductos->fetch(PDO::FETCH_ASSOC)['total'];
+    // Consulta para obtener el número total de productos
+    $sqlTotalProductos = "SELECT COUNT(*) as total FROM anuncio WHERE validado = '1'";
+
+    // Si hay una búsqueda, ajusta la consulta para buscar por nombre_anuncio
+    if (!empty($busqueda)) {
+        $sqlTotalProductos .= " AND nombre_anuncio LIKE :busqueda";
+    }
+
+    $stmtTotalProductos = $conn->prepare($sqlTotalProductos);
+
+    // Si hay una búsqueda, vincula el parámetro de búsqueda
+    if (!empty($busqueda)) {
+        $stmtTotalProductos->bindValue(':busqueda', '%' . $busqueda . '%', PDO::PARAM_STR);
+    }
+
+    $stmtTotalProductos->execute();
+    $totalProductos = $stmtTotalProductos->fetchColumn();
 
     $paginasTotales = ceil($totalProductos / $elementosPorPagina);
 
-    // Consulta para obtener los productos de la página actual
+    // Define $inicio aquí
     $inicio = ($paginaActual - 1) * $elementosPorPagina;
-    $sqlProductos = "SELECT * FROM anuncio WHERE validado = '1' LIMIT $inicio, $elementosPorPagina";
-    $resultProductos = $conn->query($sqlProductos);
+
+    // Consulta para obtener los productos de la página actual
+    $sqlProductos = "SELECT * FROM anuncio WHERE validado = '1'";
+
+    // Si hay una búsqueda, ajusta la consulta para buscar por nombre_anuncio
+    if (!empty($busqueda)) {
+        $sqlProductos .= " AND nombre_anuncio LIKE :busqueda";
+    }
+
+    $sqlProductos .= " LIMIT $inicio, $elementosPorPagina";
+
+    $stmtProductos = $conn->prepare($sqlProductos);
+
+    // Si hay una búsqueda, vincula el parámetro de búsqueda
+    if (!empty($busqueda)) {
+        $stmtProductos->bindValue(':busqueda', '%' . $busqueda . '%', PDO::PARAM_STR);
+    }
+
+    $stmtProductos->execute();
 ?>
 
 <!DOCTYPE html>
@@ -37,11 +70,37 @@
     <?php
         include("header.php");
     ?>
+    <div id="buscador">
+        <form method="GET" action="anuncio.php" id="search-form">
+            <input onkeyup="submitForm()" id="inputBuscador" type="text" name="busqueda" placeholder="Buscar por nombre de artículo 🔍" value="<?php echo $busqueda; ?>">
+        </form>
+    </div>
 
-    <section>
+    <script>
+        var inputBuscador = document.getElementById("inputBuscador");
+
+        window.addEventListener('load', function() {
+            inputBuscador.focus();
+
+            // Colocar el foco al final del campo de entrada
+            inputBuscador.setSelectionRange(inputBuscador.value.length, inputBuscador.value.length);
+        });
+        function submitForm() {
+            // Obtener el formulario por su ID
+            var form = document.getElementById("search-form");
+            // Enviar el formulario
+            form.submit();
+        }
+    </script>
         <div class="productos">
             <?php
-                while ($row = $resultProductos->fetch(PDO::FETCH_ASSOC)) {
+            if($totalProductos === 0){
+                echo '<div>';
+                echo'<p  id="mensajeBusqueda"> Lo sentimos, su busqueda "<b>'.$busqueda.'</b>" no se ha encontrado </p>';
+                echo '</div>';
+
+            }
+                while ($row = $stmtProductos->fetch(PDO::FETCH_ASSOC)) {
                     echo '<div class="producto">';
                     $imagenAlt = empty($row['foto']) ? 'Sin Foto' : ucfirst($row['nombre_anuncio']);
                     echo '<div class = "imagen-producto">';
@@ -60,20 +119,14 @@
     </section>
     <div id="paginacion">
         <a href="?pagina=<?php echo $paginaActual - 1; ?>" class="botonesPagina <?php if ($paginaActual <= 1) echo 'a-disabled'; ?>">← Anterior</a>
-
+        
         <?php for ($i = 1; $i <= $paginasTotales; $i++): ?>
-            <?php if ($i == $paginaActual): ?>
-                <a class="botonesPagina a-disabled" href="?pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
-            <?php else: ?>
-                <a class="botonesPagina" href="?pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
-            <?php endif; ?>
+            <a class="botonesPagina <?php if ($i == $paginaActual) echo 'a-disabled'; ?>" href="?pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
         <?php endfor; ?>
 
-        <a class="botonesPagina <?php if ($paginaActual >= $paginasTotales) echo 'a-disabled'; ?>" href="?pagina=<?php echo $paginaActual + 1; ?>">Siguiente →</a>
+        <a href="?pagina=<?php echo $paginaActual + 1; ?>" class="botonesPagina <?php if ($paginaActual >= $paginasTotales) echo 'a-disabled'; ?>">Siguiente →</a>
     </div>
-    
-    <?php
-        include('footer.php');
-    ?>
+
+    <?php include('footer.php'); ?>
 </body>
 </html>
