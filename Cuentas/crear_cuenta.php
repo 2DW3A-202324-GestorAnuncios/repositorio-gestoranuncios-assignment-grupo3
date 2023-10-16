3f91ce190393b7e3cadb6ce46d374a3ec44dbe60
ekaitzanguloo <ekaitzanguloo@gmail.com>
2023-10-16 09:49:07 +0200

    Revert "merge dwayne y el mip"
    This reverts commit 7aae42ef4f2a0042015acc7d5aa4fe507f69f294, reversing
    changes made to d16bbd22c5d3f0d433842a89edd297801ff49e29.

--- Cuentas/crear_cuenta.php
+++ Cuentas/crear_cuenta.php
@@ -2,138 +2,97 @@
 <html lang="es-Es">
 <head>
     <meta charset="UTF-8">
-    <script src="../script.js"></script>
     <link rel="stylesheet" href="../style.css?v=<?php echo time(); ?>">
     <link rel="shortcut icon" href="../img/favicon.png">
-    <title>Creacion de Cuenta - CIFP Txurdinaga</title>
+    <title>Crear Cuenta - CIFP Txurdinaga</title>
 </head>
 <body>
     <?php
-        session_start();
-        $usuErr = $emailErr = $contra1Err = $contra2Err = $termsErr = "";
-        $usu = $email = $contra1 = $contra2 = $terms = $action = "";
-        
-        if (isset($_POST['submit'])) {
-            if (empty($_POST["usuario"])) {
-                $usuErr = "Introduce un usuario";
-                $_SESSION['usu_vacio'] = $usuErr;
-            } else {
-                $usu = test_input($_POST["usuario"]);
-                if (!preg_match("/^[a-zA-Z0-9]*$/",$usu)) {
-                $usuErr = "Introduce solo letras y numeros";
-                }
-            }
-            
-            if (empty($_POST["email"])) {
-                $emailErr = "Introduce un email";
-            } else {
-                $email = test_input($_POST["email"]);
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Formato de email incorrecto";
-                }
-            }
-                
-            if (empty($_POST["contraseña1"])) {
-                $contra1Err = "Introduce una contraseña";
-            } else {
-                $contra1 = test_input($_POST["contraseña1"]);
-            }
-            if (($_POST["contraseña1"])!=($_POST["contraseña2"])) {
-                $contra2Err = "Las contraseñas no coinciden";
-            }else{
-                $contr2 = test_input($_POST["contraseña2"]);
+    session_start();
+    
+    $usuErr = $emailErr = $contra1Err = $contra2Err = $termsErr = "";
+    $usu = $email = $contra1 = $contra2 = $terms = "";
 
-            }
-            if (empty($_POST["contraseña2"])) {
-                $contra2Err = "Confirma la contraseña";
-            }
-
-            if (!isset($_POST["terminos"])) {
-                $termsErr = "Debe aceptar los terminos y condiciones";
-            } else {
-                $terms = test_input($_POST["terminos"]);
-                $termsErr = "";
-
-            }
-
+    if (isset($_POST['submit'])) {
+    if (empty($_POST["usuario"])) {
+        $usuErr = "Introduce un usuario";
+        $_SESSION['usu_vacio'] = $usuErr;
+    } else {
+        $usu = test_input($_POST["usuario"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z0-9]*$/",$usu)) {
+        $usuErr = "Introduce solo letras y numeros";
         }
-
-        function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-        
-        if($usuErr="" && $emailErr="" && $contra1Err="" && $contra2Err="" && $termsErr=""){
-
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "myDBPDO";
-
-            try {
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $sql = "INSERT INTO info (nombre, email, contraseña)
-                VALUES ('$nom', '$email', '$contra1')";
-                $conn->exec($sql);
-                echo "Datos eviados";
-            }
-            catch(PDOException $e){
-                echo $sql . "<br>" . $e->getMessage();
-            }
-
-        $conn = null;
+    }
+    
+    if (empty($_POST["email"])) {
+        $emailErr = "Introduce un email";
+    } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Formato de email incorrecto";
         }
+    }
+        
+    if (empty($_POST["contraseña1"])) {
+        $contra1Err = "Introduce una contraseña";
+    } else {
+        
+    }
+
+    if (($_POST["contraseña1"])!=($_POST["contraseña2"])) {
+        $contra2Err = "Las contraseñas no coinciden";
+    }
+
+    if (!isset($_POST["terminos"])) {
+        $termsErr = "Debe aceptar los terminos y condiciones";
+    } else {
+        $termsErr = "";
+        ;
+    }
+    }
+
+    function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
     }
     ?>
     <div class="form_crear_cuenta">
-        <img src="../img/Logo_Inicio_Sesion.png" alt="logo" class="logo_inicio_sesion">
-        <form  method="post" id="id_form">
+        <img src="../img/Logo_Inicio_Sesion.png" alt="Logo CIFP Txurdinaga" class="logo_inicio_sesion">
+        <form action=""  method="post" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>>
             <div class="crear_cuenta">
                 <div>
                     <p>Usuario:</p>
-                    <input type="text" class="input_text" autofocus maxlength="15" value="<?php echo $usu;?>" name="usuario" id="usuario"><br>
-                    <span class="error" id="error1"></span>
+                    <input type="text" class="input_text" maxlength="15" value="<?php echo $usu;?>" name="usuario"><br>
+                    <span class="error"> <?php if(isset($_SESSION['usu_vacio'])){echo $_SESSION['usu_vacio'];unset($_SESSION['usu_vacio']);}?></span>
                 </div>
                 <div>
                     <p>Correo:</p>
-                    <input type="mail" class="input_text" value="<?php echo $email;?>" name="email" id="email"><br>
-                    <span class="error" id="error2"></span>
+                    <input type="mail" class="input_text" value="<?php echo $email;?>" name="email"><br>
+                    <span class="error"> <?php echo $emailErr;?></span>
                 </div>
                 <div>
                     <p>Contraseña:</p>
-                    <input type="password"  class="input_text" name="contraseña1" value="<?php echo $contra1;?>" name="contraseña1" aria-laballedby="password" id="validar_contraseña">
-                    <span class="error" id="error3"></span>
-                    <div id="expresiones">
-
-                    </div>
+                    <input type="text" class="input_text" name="contraseña1" value="<?php echo $contra1;?>" name="contraseña1"><br>
+                    <span class="error"> <?php echo $contra1Err;?></span>
                 </div>
                 <div>
                     <p>Confirmar contraseña:</p>
-                    <input type="password" class="input_text" name="contraseña2" value="<?php echo $contra2;?>" name="contraseña2" id="validar_contraseña2"><br>
-                    <span class="error" id="error4"></span>
-
+                    <input type="text" class="input_text" name="contraseña2" value="<?php echo $contra2;?>" name="cotnraseña2"><br>
+                    <span class="error"> <?php echo $contra2Err;?></span>
                 </div>
             </div>
             <br>
-            
             <div class="terminos_crear_cuenta">
-                <input type="checkbox" name="terminos" class="terminos_checkbox" id="terminos_crear_cuenta" value="<?php echo $terms;?>" name="terminos" id="terminos" >
+                <input type="checkbox" name="terminos" class="terminos_checkbox" id="terminos_crear_cuenta" value="<?php echo $terms;?>" name="terminos">
                 <label class="terminos_checkbox" for="terminos_crear_cuenta">Acepto los terminos y condiciones</label><br><br>
-                <span class="error" id="error5"></span>
-
+                <span class="error"> <?php echo $termsErr;?></span>
             </div>
-            <button type="submit" value="Crear Cuenta" name="submit" class="boton" id="boton" onclick="validarCampos()">Crear Cuenta</button>
-
+            <input type="submit" value="Crear Cuenta" name="submit" class="boton">
         </form>
-            
     </div>
-    
-    <script>
-        if ( window.history.replaceState ) {
-        window.history.replaceState( null, null, window.location.href );
-        }        
-    </script>
 </body>
 </html>
--- crear_anuncio.php
+++ crear_anuncio.php
@@ -3,68 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <link rel="preload" as="style" href="hojaEstilos/fuentes.css">
+    <link rel="stylesheet" href="hojaEstilos/fuentes.css">
     <link rel="stylesheet" href="hojaEstilos/estilos.css">
     <link rel="shortcut icon" href="img/favicon.png">
-    <title>Crear Publicación - CIFP Txurdinaga</title>
+    <title>Crear Anuncio - CIFP Txurdinaga</title>
 </head>
 <body>
-    <div class="container">
-        
-    </div>
-<body>
     <?php
-        include('header.php');
+        include("header.php");
     ?>
-
-    <div class="grid_crear">
-        <div class="barra_lateral">
-
-        </div>
-        <div>
-            <form class="crear_publicacion">
-                <div class="creacion_p1">
-                    <div>
-                        <p>Titulo:</p>
-                        <input type="text" class="input_titulo">
-                    </div>
-                    <div>
-                        <p class="texto_insertar_imagen">Imagen:</p>
-                        <div class="seleccionar_imagen">
-                            <input type="file" accept="image/*" id="imagen" onchange="loadFile(event)">
-                        </div>
-                        <img id="output"/>
-
-                    </div>
-                </div>
-                <p>Descripcion:</p>
-                <div class="comment">
-                    <textarea class="descripcion" placeholder="Comment"></textarea>
-                </div>
-                <p class="texto_anuncio">Es un anuncio?</p>                
-                <div class="creacion_p3">
-                    <div>
-                        <input type="button" value="Crear" class="boton" >
-                    </div>
-
-            
-                </div>
-            </form>
-        </div>
-    </div>
+    
+    <main>
+        <section class="crear-anuncio">
+            <h1>Crear un Anuncio</h1>
+            <div class="form-crear-anuncio">
+                <form action="#" method="post" enctype="multipart/form-data">
+                    <label for="titulo">Título:</label>
+                    <input type="text" id="titulo" name="titulo" required>
+                    
+                    <label for="descripcion">Descripción:</label>
+                    <textarea id="descripcion" name="descripcion" rows="4" required></textarea>
+                    
+                    <label for="imagen">Imagen:</label>
+                    <input type="file" id="imagen" name="imagen" accept="image/*" required>
+                    
+                    <button type="submit">Crear Anuncio</button>
+                </form>
+            </div>
+        </section>
+    </main>
 
     <?php
         include('footer.php');
     ?>
 </body>
-<script>
-    var loadFile = function(event) {
-        var output = document.getElementById('output');
-        output.src = URL.createObjectURL(event.target.files[0]);
-        output.onload = function() {
-            URL.revokeObjectURL(output.src) 
-        }
-    };
-</script>
 </html>
