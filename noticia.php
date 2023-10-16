c0e3f7b056ab8020118b985a2a4fa7f1ee0372cb
aaketza <aketzacb@gmail.com>
2023-10-16 10:51:19 +0200

    modifcacion en contacto, cambios  filtro noticias

--- noticia.php
+++ noticia.php
@@ -6,45 +6,49 @@
 ?>
 <!DOCTYPE html>
 <html lang="es-Es">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="preload" as="style" href="hojaEstilos/fuentes.css">
-	<link rel="stylesheet" href="hojaEstilos/fuentes.css">
-	<link rel="stylesheet" href="hojaEstilos/estilos.css">
-	<link rel="shortcut icon" href="img/favicon.png">
-	<title>Noticias - CIFP Txurdinaga</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preload" as="style" href="hojaEstilos/fuentes.css">
+    <link rel="stylesheet" href="hojaEstilos/fuentes.css">
+    <link rel="stylesheet" href="hojaEstilos/estilos.css">
+    <link rel="shortcut icon" href="img/favicon.png">
+    <title>Noticias - CIFP Txurdinaga</title>
 </head>
+
 <body>
     <?php
         include("header.php");
     ?>
 
     <div class="container">
-        <div class="filtro">
-            <h3>Filtrar por Categoría</h3>
-            <form id="filtroForm">
-                <label class="filtro-label">
-                    <input type="radio" name="categoria" id="categoria-deportes" value="deportes">
-                    <img src="img/categorias_noticias/deporte.png" alt="Deportes">
-                    <h5>Deportes</h5>
-                </label><br>
-                <label class="filtro-label">
-                    <input type="radio" name="categoria" id="categoria-economia" value="economia">
-                    <img src="img/categorias_noticias/economia.png" alt="Economía">
-                    <h5>Economía</h5>
-                </label><br>
-                <label class="filtro-label">
-                    <input type="radio" name="categoria" id="categoria-arte" value="arte">
-                    <img src="img/categorias_noticias/arte.png" alt="Arte">
-                    <h5>Arte</h5>
-                </label><br>
-                <label class="filtro-label">
-                    <input type="radio" name="categoria" id="categoria-tiempo" value="tiempo">
-                    <img src="img/categorias_noticias/tiempo.png" alt="Tiempo">
-                    <h5>Tiempo</h5>
-                </label><br>
-            </form>
+        <div class="filtro-container">
+            <div class="filtro">
+                <h3>Filtrar por Categoría</h3><br><br>
+                <form id="filtroForm">
+                    <label class="filtro-label">
+                        <input type="radio" name="categoria" id="categoria-deportes" value="deportes">
+                        <img src="img/categorias_noticias/deporte.png" alt="Deportes">
+                        <h5 style="margin-top: 5px;">Deportes</h5>
+                    </label><br>
+                    <label class="filtro-label">
+                        <input type="radio" name="categoria" id="categoria-economia" value="economia">
+                        <img src="img/categorias_noticias/economia.png" alt="Economía">
+                        <h5 style="margin-top: 5px;">Economía</h5>
+                    </label><br>
+                    <label class="filtro-label">
+                        <input type="radio" name="categoria" id="categoria-arte" value="arte">
+                        <img src="img/categorias_noticias/arte.png" alt="Arte">
+                        <h5 style="margin-top: 5px;">Arte</h5>
+                    </label><br>
+                    <label class="filtro-label">
+                        <input type="radio" name="categoria" id="categoria-tiempo" value="tiempo">
+                        <img src="img/categorias_noticias/tiempo.png" alt="Tiempo">
+                        <h5 style="margin-top: 5px;">Tiempo</h5>
+                    </label><br>
+                </form>
+            </div>
         </div>
         <div class="noticias3">
             <h2>Noticias</h2>
@@ -52,8 +56,9 @@
                 <?php
                     while ($row = $resultNoticias->fetch(PDO::FETCH_ASSOC)) {
                         echo '<div class="noticia3 categoria-' . $row['categoria'] . '">';
-                        echo '<h1 class="titulo-noticia3-h1">' . $row['categoria'] . '</h1>';
+                        
                         echo '<img src="img/noticias/' . $row['foto'] . '" alt="' . htmlspecialchars($row['titulo']) . '" class="imagen-noticia3">';
+                        echo '<h1 class="titulo-noticia3-h1">' . $row['categoria'] . '</h1>';
                         echo '<h2 class="titulo-noticia3">' . $row['titulo'] . '</h2>';
                         echo '</div>';
                     }
@@ -66,30 +71,31 @@
         include('footer.php');
     ?>
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            // Obtén una referencia al formulario de filtro
-            const filtroForm = document.getElementById("filtroForm");
+    document.addEventListener("DOMContentLoaded", function() {
+        // Obtén una referencia al formulario de filtro
+        const filtroForm = document.getElementById("filtroForm");
 
-            // Agrega un evento de cambio al formulario
-            filtroForm.addEventListener("change", function () {
-                // Obtén el valor de la categoría seleccionada
-                const selectedCategoria = document.querySelector('input[name="categoria"]:checked').value;
+        // Agrega un evento de cambio al formulario
+        filtroForm.addEventListener("change", function() {
+            // Obtén el valor de la categoría seleccionada
+            const selectedCategoria = document.querySelector('input[name="categoria"]:checked').value;
 
-                // Obtén todas las noticias
-                const noticias = document.querySelectorAll('.noticia3');
+            // Obtén todas las noticias
+            const noticias = document.querySelectorAll('.noticia3');
 
-                // Recorre todas las noticias y ocúltalas si no coinciden con la categoría seleccionada
-                noticias.forEach(function (noticia) {
-                    const categoriaNoticia = noticia.className.match(/categoria-(\w+)/)[1];
+            // Recorre todas las noticias y ocúltalas si no coinciden con la categoría seleccionada
+            noticias.forEach(function(noticia) {
+                const categoriaNoticia = noticia.className.match(/categoria-(\w+)/)[1];
 
-                    if (selectedCategoria === "todas" || selectedCategoria === categoriaNoticia) {
-                        noticia.style.display = "block";
-                    } else {
-                        noticia.style.display = "none";
-                    }
-                });
+                if (selectedCategoria === "todas" || selectedCategoria === categoriaNoticia) {
+                    noticia.style.display = "block";
+                } else {
+                    noticia.style.display = "none";
+                }
             });
         });
+    });
     </script>
 </body>
+
 </html>
