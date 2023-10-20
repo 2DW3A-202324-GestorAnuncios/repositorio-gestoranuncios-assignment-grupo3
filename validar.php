df8f4503ff0c475401fa2ea3c9202e161f9dffce
ekaitzanguloo <ekaitzanguloo@gmail.com>
2023-10-20 13:30:08 +0200

    Merge remote-tracking branch 'origin/luka' into ekaitz con errores

--- validar.php
+++ validar.php
@@ -1,74 +1,73 @@
 <?php
-include("conexion.php");
+    include("conexion.php");
 
-function eliminarFoto($nombreArchivo) {
-    $directorio_destino = './img/anuncios/' . $nombreArchivo;
-    if (file_exists($directorio_destino)) {
-        unlink($directorio_destino); // Borra el archivo
+    function eliminarFoto($nombreArchivo) {
+        $directorio_destino = './img/anuncios/' . $nombreArchivo;
+        if (file_exists($directorio_destino)) {
+            unlink($directorio_destino); // Borra el archivo
+        }
     }
-}
 
-$sqlNoticias = "SELECT * FROM noticia WHERE validado = '0'";
-$resultNoticias = $conn->query($sqlNoticias);
-
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["validar_noticia"])) {
-    $id_noticia = $_POST["validar_noticia"];
-
-    // Realiza una consulta SQL para actualizar el campo validado a 1
-    $sqlValidarNoticia = "UPDATE noticia SET validado = '1' WHERE id_noticia = :id_noticia";
-
-    $stmt = $conn->prepare($sqlValidarNoticia);
-    $stmt->bindValue(':id_noticia', $id_noticia, PDO::PARAM_INT);
-    $stmt->execute();
+    $sqlNoticias = "SELECT * FROM noticia WHERE validado = '0'";
     $resultNoticias = $conn->query($sqlNoticias);
-}
 
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["eliminar_noticia"])) {
-    $id_noticia = $_POST["eliminar_noticia"];
-    $sqlEliminarNoticia = "DELETE FROM noticia WHERE id_noticia = :id_noticia";
-    $stmt = $conn->prepare($sqlEliminarNoticia);
-    $stmt->bindParam(':id_noticia', $id_noticia, PDO::PARAM_INT);
-    $stmt->execute();
-    $resultNoticias = $conn->query($sqlNoticias);
-}
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["validar_noticia"])) {
+        $id_noticia = $_POST["validar_noticia"];
 
-$sqlAnuncios = "SELECT * FROM anuncio WHERE validado = '0'";
-$resultAnuncios = $conn->query($sqlAnuncios);
+        // Realiza una consulta SQL para actualizar el campo validado a 1
+        $sqlValidarNoticia = "UPDATE noticia SET validado = '1' WHERE id_noticia = :id_noticia";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["validar_anuncio"])) {
-    $id_anuncio = $_POST["validar_anuncio"];
+        $stmt = $conn->prepare($sqlValidarNoticia);
+        $stmt->bindValue(':id_noticia', $id_noticia, PDO::PARAM_INT);
+        $stmt->execute();
+        $resultNoticias = $conn->query($sqlNoticias);
+    }
 
-    // Realiza una consulta SQL para actualizar el campo validado a 1
-    $sqlValidarAnuncio = "UPDATE anuncio SET validado = '1' WHERE id_anuncio = :id_anuncio";
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["eliminar_noticia"])) {
+        $id_noticia = $_POST["eliminar_noticia"];
+        $sqlEliminarNoticia = "DELETE FROM noticia WHERE id_noticia = :id_noticia";
+        $stmt = $conn->prepare($sqlEliminarNoticia);
+        $stmt->bindParam(':id_noticia', $id_noticia, PDO::PARAM_INT);
+        $stmt->execute();
+        $resultNoticias = $conn->query($sqlNoticias);
+    }
 
-    $stmt = $conn->prepare($sqlValidarAnuncio);
-    $stmt->bindValue(':id_anuncio', $id_anuncio, PDO::PARAM_INT);
-    $stmt->execute();
+    $sqlAnuncios = "SELECT * FROM anuncio WHERE validado = '0'";
     $resultAnuncios = $conn->query($sqlAnuncios);
 
-}
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["validar_anuncio"])) {
+        $id_anuncio = $_POST["validar_anuncio"];
 
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["eliminar_anuncio"])) {
-    $id_anuncio = $_POST["eliminar_anuncio"];
-    $sqlFoto = "SELECT foto FROM anuncio WHERE id_anuncio = :id_anuncio";
-    $stmtFoto = $conn->prepare($sqlFoto);
-    $stmtFoto->bindValue(':id_anuncio', $id_anuncio, PDO::PARAM_INT);
-    $stmtFoto->execute();
-    $sacarFoto = $stmtFoto->fetch();
+        // Realiza una consulta SQL para actualizar el campo validado a 1
+        $sqlValidarAnuncio = "UPDATE anuncio SET validado = '1' WHERE id_anuncio = :id_anuncio";
 
-    $sqlEliminarAnuncio = "DELETE FROM anuncio WHERE id_anuncio = :id_anuncio";
-    $stmt = $conn->prepare($sqlEliminarAnuncio);
-    $stmt->bindParam(':id_anuncio', $id_anuncio, PDO::PARAM_INT);
-    $stmt->execute();
-
-    $resultAnuncios = $conn->query($sqlAnuncios);
-
-    // Obtener el nombre del archivo de la base de datos
-    // Verificar si la consulta fue exitosa
-    if ($sacarFoto && isset($sacarFoto['foto'])) {
-        eliminarFoto($sacarFoto['foto']); // Llama a la función eliminarFoto
+        $stmt = $conn->prepare($sqlValidarAnuncio);
+        $stmt->bindValue(':id_anuncio', $id_anuncio, PDO::PARAM_INT);
+        $stmt->execute();
+        $resultAnuncios = $conn->query($sqlAnuncios);
     }
-}
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["eliminar_anuncio"])) {
+        $id_anuncio = $_POST["eliminar_anuncio"];
+        $sqlFoto = "SELECT foto FROM anuncio WHERE id_anuncio = :id_anuncio";
+        $stmtFoto = $conn->prepare($sqlFoto);
+        $stmtFoto->bindValue(':id_anuncio', $id_anuncio, PDO::PARAM_INT);
+        $stmtFoto->execute();
+        $sacarFoto = $stmtFoto->fetch();
+
+        $sqlEliminarAnuncio = "DELETE FROM anuncio WHERE id_anuncio = :id_anuncio";
+        $stmt = $conn->prepare($sqlEliminarAnuncio);
+        $stmt->bindParam(':id_anuncio', $id_anuncio, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $resultAnuncios = $conn->query($sqlAnuncios);
+
+        // Obtener el nombre del archivo de la base de datos
+        // Verificar si la consulta fue exitosa
+        if ($sacarFoto && isset($sacarFoto['foto'])) {
+            eliminarFoto($sacarFoto['foto']); // Llama a la función eliminarFoto
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="es-Es">
@@ -103,12 +102,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["eliminar_anuncio"])) 
         </div>
         <div class="productos">
             <?php
-            while ($row = $resultAnuncios->fetch(PDO::FETCH_ASSOC)) {
-                $nombre_anuncio = $row['nombre_anuncio'];
-                echo '<div class="producto">';
-                $imagenAlt = empty($row['foto']) ? 'Sin Foto' : ucfirst($row['nombre_anuncio']);
-                    echo '<form method="POST" action="validar.php">';
-                        echo '<div class="imagen-validar">';
+                while ($row = $resultAnuncios->fetch(PDO::FETCH_ASSOC)) {
+                    $nombre_anuncio = $row['nombre_anuncio'];
+                    $imagenAlt = empty($row['foto']) ? 'Sin Foto' : ucfirst($row['nombre_anuncio']);
+                    echo '<form class="producto" method="POST" action="validar.php">';
+                        echo '<div class="imagen-producto">';
                             echo '<img src="img/anuncios/' . $row['foto'] . '" alt="' . htmlspecialchars($imagenAlt) . '">';
                         echo '</div>';
                         echo '<div class="contenedor-anuncio">';
@@ -116,38 +114,33 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["eliminar_anuncio"])) 
                             echo '<p>' . $row['descripcion'] . '</p>';
                             echo '<p class="precio">' . $row['precio'] . '€</p>';
                         echo '</div>';
-                            echo '<button type = "button" style="background-color: #57aa26" id="confirmacionA">Validar</button>';
-                                echo ' 
-                                <div id="confirmar-cierre" class="modalValidarA" style="display: none;">
-                                    <div class="modal-content" display ="none">
-                                        <p>¿Esta seguro de validar el anuncio?</p>
-                                        <button id="confirmar-si" name="validar_anuncio" value="' . $row['id_anuncio'] . '">Sí</button>
-                                        <button type = "button" id="confirmar-no-validarA" >No</button>
-                                    </div>
-                                </div>
-                                ';
-                            echo '<br>';
-                            echo '<br>';
-                            echo '<button  type = "button" style="background-color: red" id="confirmacionEliminarA">Eliminar</button>';
-                            echo ' 
-                                <div id="confirmar-cierre" class="modalEliminarA" style="display: none;">
-                                    <div class="modal-content" display ="none">
-                                        <p>¿Esta seguro de eliminar el anuncio?</p>
-                                        <button id="confirmar-si" name="eliminar_anuncio" value="' . $row['id_anuncio'] . '">Sí</button>
-                                        <button type = "button" id="confirmar-no-eliminarA" >No</button>
-                                    </div>
-                                </div>
-                                ';
+                        echo '<div class="btn-container">';
+                            echo '<button type="button" name="validar-anuncio" id="confirmacionA">Validar</button>';
+                            echo '<div id="confirmar-cierre" class="modalValidarA" style="display: none;">';
+                                echo '<div class="modal-content" display="none">';
+                                    echo '<p>¿Esta seguro de validar el anuncio?</p>';
+                                    echo '<button id="confirmar-si" name="validar_anuncio" value="' . $row['id_anuncio'] . '">Sí</button>';
+                                    echo '<button type="button" id="confirmar-no-validarA">No</button>';
+                                echo '</div>';
+                            echo '</div>';
+                            echo '<button type="button" name="eliminar-anuncio" id="confirmacionEliminarA">Eliminar</button>';
+                            echo '<div id="confirmar-cierre" class="modalEliminarA" style="display: none;">';
+                                echo '<div class="modal-content" display="none">';
+                                    echo '<p>¿Esta seguro de eliminar el anuncio?</p>';
+                                    echo '<button id="confirmar-si" name="eliminar_anuncio" value="' . $row['id_anuncio'] . '">Sí</button>';
+                                    echo '<button type ="button" id="confirmar-no-eliminarA">No</button>';
+                                echo '</div>';
+                            echo '</div>';
+                        echo '</div>';
                     echo '</form>';
-                echo '</div>';
-            }
+                }
             ?>
         </div>
     </section>
     <br>
     <br>
     <section class="seccion-destacada">
-        <div class="seccion-titulo" >
+        <div class="seccion-titulo">
             <h1 class="titulo-llamativo">Validación de Noticias</h1>
         </div>
         <div id="noticiasContainer" class="productos">
@@ -162,26 +155,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["eliminar_anuncio"])) 
                             echo '<h2 class="titulo-noticia3">' . $row['titulo'] . '</h2>';
                         echo '</div>';
                         echo '<div class="btn-container">';
-                            echo '<button type = "button" style="background-color: #57aa26" id="confirmacionN">Validar</button>';
-                            echo ' 
-                            <div id="confirmar-cierre" class="modalValidarN" style="display: none;">
-                                <div class="modal-content" display ="none">
-                                    <p>¿Esta seguro de validar el anuncio?</p>
-                                    <button id="confirmar-si" name="validar_noticia" value="' . $row['id_noticia'] . '">Sí</button>
-                                    <button type = "button" id="confirmar-no-validarN" >No</button>
-                                </div>
-                            </div>
-                            ';
-                            echo '<button type= "button" style="background-color: red" id="confirmacionEliminarN" >Eliminar</button>';
-                            echo ' 
-                            <div id="confirmar-cierre" class="modalEliminarN"  style="display: none;">
-                                <div class="modal-content" display ="none">
-                                    <p>¿Esta seguro de eliminar el anuncio?</p>
-                                    <button id="confirmar-si" name="eliminar_noticia" value="' . $row['id_noticia'] . '">Sí</button>
-                                    <button type = "button" id="confirmar-no-eliminarN" >No</button>
-                                </div>
-                            </div>
-                            ';
+                            echo '<button type="button" name="validar-noticia" id="confirmacionN">Validar</button>';
+                            echo '<div id="confirmar-cierre" class="modalValidarN" style="display: none;">';
+                                echo '<div class="modal-content" display="none">';
+                                    echo '<p>¿Esta seguro de validar el anuncio?</p>';
+                                    echo '<button id="confirmar-si" name="validar_noticia" value="' . $row['id_noticia'] . '">Sí</button>';
+                                    echo '<button type="button" id="confirmar-no-validarN">No</button>';
+                                echo '</div>';
+                            echo '</div>';
+                            echo '<button type="button" name="eliminar-noticia" id="confirmacionEliminarN">Eliminar</button>';
+                            echo '<div id="confirmar-cierre" class="modalEliminarN"  style="display: none;">';
+                                echo '<div class="modal-content" display="none">';
+                                    echo '<p>¿Esta seguro de eliminar el anuncio?</p>';
+                                    echo '<button id="confirmar-si" name="eliminar_noticia" value="' . $row['id_noticia'] . '">Sí</button>';
+                                    echo '<button type="button" id="confirmar-no-eliminarN">No</button>';
+                                echo '</div>';
+                            echo '</div>';
                         echo '</div>';
                     echo '</form>';
                 }
