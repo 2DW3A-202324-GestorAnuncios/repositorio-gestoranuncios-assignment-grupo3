6a873df690b81c0ae26dc435e1c11b0211acb05f
LukaCarmona <lukacarmona115@gmail.com>
2023-10-24 13:09:18 +0200

    paginacion top top

--- anuncio.php
+++ anuncio.php
@@ -131,11 +131,23 @@
 
     <div id="paginacion">
         <a href="?pagina=<?php echo $paginaActual - 1; ?>" class="botonesPagina <?php if ($paginaActual <= 1) echo 'a-disabled'; ?>">← Anterior</a>
-        
-        <?php for ($i = 1; $i <= $paginasTotales; $i++): ?>
-            <a class="botonesPagina <?php if ($i == $paginaActual) echo 'a-disabled'; ?>" href="?pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
-        <?php endfor; ?>
+            <?php
+            // Obtener la página actual
+            $paginaActual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
 
+            // Calcular el número total de páginas (supongamos que tienes esto en $paginasTotales)
+
+            // Mostrar la página actual más 2 y la última página
+            echo '<div id="paginacion">';
+            for ($i = max(1, $paginaActual - 1); $i <= min($paginaActual + 1, $paginasTotales); $i++) {
+                echo '<a class="botonesPagina ' . ($i == $paginaActual ? 'a-disabled' : '') . '" href="?pagina=' . $i . '">' . $i . '</a>';
+            }
+            if ($paginaActual < $paginasTotales - 1) {
+                echo '...';
+                echo '<a class="botonesPagina" href="?pagina=' . $paginasTotales . '">' . $paginasTotales . '</a>';
+            }
+            echo '</div>';
+            ?>
         <a href="?pagina=<?php echo $paginaActual + 1; ?>" class="botonesPagina <?php if ($paginaActual >= $paginasTotales) echo 'a-disabled'; ?>">Siguiente →</a>
     </div>
 
