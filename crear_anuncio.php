a4e6404ab61b261591a8075d2f2a14e40ff498a9
aaketza <aketzacb@gmail.com>
2023-10-30 12:26:24 +0100

    validacion en crear publicaciones

--- crear_anuncio.php
+++ crear_anuncio.php
@@ -1,8 +1,8 @@
 <?php
-    include("conexion.php");
-        
-    // Inicia la sesión en la página
-    session_start();
+include("conexion.php");
+
+// Inicia la sesión en la página
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +30,9 @@
         $mensaje_error = '';
         $usuario = $_SESSION["usuarioLogin"];
 
+        $nomAnuncio = '';
+        $descAnuncio = '';
+        
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             //coje los elementos del formulario
             $nomAnuncio = $_POST['titulo'];
@@ -41,52 +44,63 @@
 
             $directorio_destino = 'img/anuncios/' . $fotoAnuncio;
 
-            if(empty($nomAnuncio) && empty($descAnuncio)){
-                $mensaje_error = "Debes introducir el titulo y la descripcion del anuncio.";
-            }else if(empty($nomAnuncio)){
-                $mensaje_error = "Debes introducir un titulo del anuncio.";
-            }else if(empty($descAnuncio)){
-                $mensaje_error = "Debes introducir una descripcion del anuncio.";
-            }else if (!empty($fotoAnuncio)) {
-                if (move_uploaded_file($foto_temp, $directorio_destino)) {
-                    // Inserta los datos a la tabla "Anuncio" con el nombre de la imagen en la base de datos
-                    $sql = "INSERT INTO anuncio (nombre_anuncio, precio, descripcion, foto, nombre_usuario) VALUES ('$nomAnuncio','$precAnuncio','$descAnuncio','$fotoAnuncio','$usuAnuncio')";
+            if (empty($nomAnuncio) && empty($descAnuncio)) {
+                $mensaje_error = "Debes introducir el título y la descripción del anuncio.";
+            } else if (empty($nomAnuncio)) {
+                $mensaje_error = "Debes introducir un título del anuncio.";
+            } else if (empty($descAnuncio)) {
+                $mensaje_error = "Debes introducir una descripción del anuncio.";
+            } else {
+                if (!empty($fotoAnuncio)) {
+                    if (move_uploaded_file($foto_temp, $directorio_destino)) {
+                        // Inserta los datos a la tabla "Anuncio" con el nombre de la imagen en la base de datos
+                        $sql = "INSERT INTO anuncio (nombre_anuncio, precio, descripcion, foto, nombre_usuario) VALUES ('$nomAnuncio','$precAnuncio','$descAnuncio','$fotoAnuncio','$usuAnuncio')";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->execute();
+                        $usuario_data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                        $mensaje_exito = "Se ha creado la publicación";
+                        
+                        // Restablecer los campos a cadenas vacías después de un envío exitoso
+                        $nomAnuncio = '';
+                        $descAnuncio = '';
+                    }
+                } else {
+                    $sql = "INSERT INTO anuncio (nombre_anuncio, precio, descripcion, nombre_usuario) VALUES ('$nomAnuncio','$precAnuncio','$descAnuncio','$usuAnuncio')";
                     $stmt = $conn->prepare($sql);
                     $stmt->execute();
                     $usuario_data = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-                    $mensaje_exito = "Se ha creado la publicación";
-                }
-            } else {
-                $sql = "INSERT INTO anuncio (nombre_anuncio, precio, descripcion, nombre_usuario) VALUES ('$nomAnuncio','$precAnuncio','$descAnuncio','$usuAnuncio')";
-                $stmt = $conn->prepare($sql);
-                $stmt->execute();
-                $usuario_data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-                $mensaje_exito = "Se ha creado la publicación";
+                    $mensaje_exito = "Se ha creado la publicación";
+                    
+                    // Restablecer los campos a cadenas vacías después de un envío exitoso
+                    $nomAnuncio = '';
+                    $descAnuncio = '';
+                }
             }
         }
-
+    
         if (!empty($mensaje_exito)) {
             echo '<div class="mensaje-exito">';
-                echo '<p><strong>Éxito!</strong> ' . $mensaje_exito . '</p>';
+            echo '<p><strong>Éxito!</strong> ' . $mensaje_exito . '</p>';
             echo '</div>';
         } else if (!empty($mensaje_error)) {
             echo '<div class="mensaje-error">';
-                echo '<p><strong>Error!</strong> ' . $mensaje_error .'</p>';
+            echo '<p><strong>Error!</strong> ' . $mensaje_error .'</p>';
             echo '</div>';
         }
     ?>
 
     <section class="crear-anuncio">
         <h1>Crear un Anuncio</h1>
+
         <div class="form-crear-anuncio">
             <form action="#" method="post" enctype="multipart/form-data">
                 <label for="titulo">Título:</label>
-                <input type="text" id="titulo" name="titulo">
+                <input type="text" id="titulo" name="titulo" value="<?php echo $nomAnuncio; ?>">
 
                 <label for="descripcion">Descripción:</label>
-                <textarea id="descripcion" name="descripcion" rows="4"></textarea>
+                <textarea id="descripcion" name="descripcion" rows="4"><?php echo $descAnuncio; ?></textarea>
 
                 <label for="imagen">Imagen:</label>
                 <input type="file" id="imagen" name="imagen" accept="image/*">
@@ -99,7 +113,7 @@
     </section>
 
     <?php
-        include('footer.php');
+    include('footer.php');
     ?>
 </body>
 
--- crear_noticia.php
+++ crear_noticia.php
@@ -1,8 +1,8 @@
 <?php
-    include("conexion.php");
-        
-    // Inicia la sesión en la página
-    session_start();
+include("conexion.php");
+
+// Inicia la sesión en la página
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -25,56 +25,55 @@
         } else {
             include('header_no_sesion.php');
         }
-        
+
         $mensaje_exito = '';
         $mensaje_error = '';
         $usuario = $_SESSION['usuarioLogin'];
 
-        $repeticionPK = "";
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $repeticionPK = '';
+        $nomNoticia = '';
+        $descNoticia = '';
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $nomNoticia = $_POST['titulo'];
             $descNoticia = $_POST['descripcion'];
             $catNoticia = $_POST['categoria'];
             $usuNoticia = $_SESSION['usuarioLogin'];
             $fotoNoticia = $_FILES['imagen']['name'];
             $foto_temp = $_FILES['imagen']['tmp_name'];
-            
+
             // Directorio de destino para la foto
             $directorio_destino = 'img/noticias/' . $fotoNoticia;
-            
-            // Mueve el archivo temporal al directorio de fotos
-            if(empty($nomNoticia) && empty($descNoticia) && empty($fotoNoticia)){
-                $mensaje_error = "Debes introducir el titulo la descripcion y la foto de la noticia.";
-            }else if(empty($nomNoticia)){
-                $mensaje_error = "Debes introducir el titulo la noticia.";
-            }else if(empty($descNoticia)){
-                $mensaje_error = "Debes introducir la descripcion de la noticia.";
-            }else if(empty($nomNoticia)  && empty($descNoticia)){
-                $mensaje_error = "Debes introducir el titulo  y la descripcion de la noticia.";
-            }else if(empty($descNoticia) && empty($fotoNoticia)){
-                $mensaje_error = "Debes introducir la descripcion y la foto de la noticia.";
-            }else if(empty($nomNoticia)  && empty($fotoNoticia)){
-                $mensaje_error = "Debes introducir el titulo  y la foto de la noticia.";
-            }else if (!empty($nomNoticia) && !empty($descNoticia) && move_uploaded_file($foto_temp, $directorio_destino)) {
+
+            if (empty($nomNoticia)) {
+                $mensaje_error = "Debes introducir el título de la noticia.";
+            } else if (empty($descNoticia)) {
+                $mensaje_error = "Debes introducir la descripción de la noticia.";
+            } else if (empty($fotoNoticia)) {
+                $mensaje_error = "Debes introducir una foto.";
+            } else if (move_uploaded_file($foto_temp, $directorio_destino)) {
                 // Inserta los datos a la tabla "noticia" con el nombre de la imagen en la base de datos
                 $sql = "INSERT INTO noticia (foto, titulo, descripcion, categoria, nombre_usuario) VALUES ('$fotoNoticia','$nomNoticia','$descNoticia','$catNoticia','$usuNoticia')";
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
                 $usuario_data = $stmt->fetch(PDO::FETCH_ASSOC);
                 $mensaje_exito = "Se ha creado la publicación";
-            } else {
-                $mensaje_error = "Debes introducir una foto.";
-            }  
-        }
 
+                // Restablecer los campos a cadenas vacías después de un envío exitoso
+                $nomNoticia = '';
+                $descNoticia = '';
+            } else {
+                $mensaje_error = "Ocurrió un error al subir la foto.";
+            }
+        }
+        
         if (!empty($mensaje_exito)) {
             echo '<div class="mensaje-exito">';
-                echo '<p><strong>Éxito!</strong> ' . $mensaje_exito . '</p>';
+            echo '<p><strong>Éxito!</strong> ' . $mensaje_exito . '</p>';
             echo '</div>';
         } else if (!empty($mensaje_error)) {
             echo '<div class="mensaje-error">';
-                echo '<p><strong>Error!</strong> ' . $mensaje_error . '</p>';
+            echo '<p><strong>Error!</strong> ' . $mensaje_error . '</p>';
             echo '</div>';
         }
     ?>
@@ -83,10 +82,10 @@
         <h1>Crear Noticia</h1>
         <form class="form-crear-noticia" method="post" enctype="multipart/form-data">
             <label for="titulo">Título:</label>
-            <input type="text" id="titulo" name="titulo">
+            <input type="text" id="titulo" name="titulo" value="<?php echo $nomNoticia; ?>">
 
             <label for="descripcion">Descripción:</label>
-            <textarea id="descripcion" name="descripcion" rows="4"></textarea>
+            <textarea id="descripcion" name="descripcion" rows="4"><?php echo $descNoticia; ?></textarea>
 
             <label for="imagen">Imagen:</label>
             <input type="file" id="imagen" name="imagen" accept="image/*">
@@ -103,8 +102,9 @@
     </section>
 
     <?php
-        include('footer.php');
+    include('footer.php');
     ?>
+
 </body>
 
 </html>
