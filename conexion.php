97e4ea912f69783ffb60d013e013691c0cb48b11
root <root@ip-172-30-2-165.ec2.internal>
2023-11-02 15:01:38 +0000

    Actualizacion de conexion al aws

--- conexion.php
+++ conexion.php
@@ -2,10 +2,10 @@
     // Especificamos los ajustes de la base de datos
     // Creamos la conexion y la metemos en una variable en español
     try {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "gestor_anuncios";
+        $servername = "gestor-aununcios-g3.cv2cmtkat6wh.us-east-1.rds.amazonaws.com";
+        $username = "admin";
+        $password = "123456789";
+        $dbname = "gestor-anuncios-g3";
 
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
