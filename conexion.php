31088086740c05175791a23d6df0602919f2b402
LukaCarmona <lukacarmona115@gmail.com>
2023-11-02 13:36:56 +0100

    conexion para localhost

--- conexion.php
+++ conexion.php
@@ -1,16 +1,11 @@
 <?php
     // Especificamos los ajustes de la base de datos
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "gestor_anuncios";
-
     // Creamos la conexion y la metemos en una variable en español
     try {
-        $servername = "base-g3.cv2cmtkat6wh.us-east-1.rds.amazonaws.com";
-        $username = "admin";
-        $password = "123456789";
-        $dbname = "gestor-anuncios-g3";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "gestor_anuncios";
 
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
