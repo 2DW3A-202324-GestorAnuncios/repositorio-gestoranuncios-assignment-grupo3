e0ca6c3de504ed208b6dc2becf2e0d64430aa575
root <root@ip-172-30-2-165.ec2.internal>
2023-11-02 15:49:19 +0000

    actualizacion de nuevo de la conexion al aws

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@
         $servername = "gestor-aununcios-g3.cv2cmtkat6wh.us-east-1.rds.amazonaws.com";
         $username = "admin";
         $password = "123456789";
-        $dbname = "gestor-anuncios-g3";
+        $dbname = "gestor_anuncios_g3";
 
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
