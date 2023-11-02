83fd31f4185f6ad5f752947a069f392397e19148
root <root@ip-172-30-2-165.ec2.internal>
2023-11-02 16:17:34 +0000

    nueva conexion

--- conexion.php
+++ conexion.php
@@ -2,13 +2,13 @@
     // Especificamos los ajustes de la base de datos
     // Creamos la conexion y la metemos en una variable en español
     try {
-        $hoatDB = "gestor-aununcios-g3.cv2cmtkat6wh.us-east-1.rds.amazonaws.com";
+        $hostDB = "gestor-aununcios-g3.cv2cmtkat6wh.us-east-1.rds.amazonaws.com";
         $nombreDB = "gestor_anuncios"
         $usuarioDB = "admin";
         $passwordDB = "123456789";
 
 	$hostPDO = "mysql:host=$hostDB;dbname=nombreDB;charset=utf8mb4";
-	$conn = new PDO($hoatPDO, $usuarioDB, $passwordDB);
+	$conn = new PDO($hostPDO, $usuarioDB, $passwordDB);
     } catch(PDOException $e) {
         // Comprobamos los errores
         echo "Error de conexión: " . $e->getMessage();
