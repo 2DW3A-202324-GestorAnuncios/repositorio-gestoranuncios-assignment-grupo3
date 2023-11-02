015d1ebdba1407d823416fd96756eed4704dee3c
root <root@ip-172-30-2-70.ec2.internal>
2023-11-02 11:16:58 +0000

    Añado cambios en conexion.php para la conexion a la base de datos en AWS

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "gestor_anuncios";
-
     try {
+        $servername = "base-g3.cv2cmtkat6wh.us-east-1.rds.amazonaws.com";
+        $username = "admin";
+        $password = "123456789";
+        $dbname = "gestor_anuncios";
+
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conn->exec("set names utf8");
