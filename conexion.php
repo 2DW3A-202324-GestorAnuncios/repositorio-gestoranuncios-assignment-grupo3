d4969175549eea23d1c8c75b2968410d94b18740
usuario <email@ejemplo.com>
2023-11-03 08:49:19 +0100

    conexion bien para el server

--- conexion.php
+++ conexion.php
@@ -1,14 +1,20 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "gestor_anuncios";
-
+    // Especificamos los ajustes de la base de datos
+    // $hostDB = "localhost";
+    // $nombreDB = "gestor_anuncios";
+    // $usuarioDB = "root";
+    // $passwordDB = "";
+    // Creamos la conexion y la metemos en una variable en español
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conn->exec("set names utf8");
+        $hostDB = "gestor-anuncios.c1hea09aphok.us-east-1.rds.amazonaws.com";
+        $nombreDB = "gestor_anuncios";
+        $usuarioDB = "admin";
+        $passwordDB = "123456789";
+
+	$hostPDO = "mysql:host=$hostDB;dbname=$nombreDB;charset=utf8mb4";
+	$conn = new PDO($hostPDO, $usuarioDB, $passwordDB);
     } catch(PDOException $e) {
+        // Comprobamos los errores
         echo "Error de conexión: " . $e->getMessage();
     }
 ?>
